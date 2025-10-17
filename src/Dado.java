3b95667f8e5c126c32818fcc531ce9df98460ec3
FELIPE LIMA RAGONHA <felipe.lr3@puccampinas.edu.br>
2025-10-17 11:05:58 -0300

    Classe Dado

---
+++ src/Dado.java
@@ -0,0 +1,8 @@
+public class Dado {
+    public static int D1(){
+        return (int)(Math.random() * (6-1+1)) + 1;
+    }
+    public static int D2(){
+        return (int)(Math.random() * (6-1+1)) + 1;
+    }
+}
