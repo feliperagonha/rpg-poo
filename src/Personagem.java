14bf50ee0e7277ecaf1f8fe954ae969a1263fcfb
Bruno Cesar <brunocesarglm@gmail.com>
2025-10-17 23:55:35 -0300

    Mudando personagem para 'abstract' e adicionando construtor do personagem

--- src/Personagem.java
+++ src/Personagem.java
@@ -1,10 +1,25 @@
-public class Personagem {
-    private String nome;
-    private byte pontosVida;
-    private int ataque;
-    private int Defesa;
-    private short nivel;
-    private Inventario inventario;
+
+/*
+ A classe personagem é uma classe abstract impedindo que nao seja possivel ter uma
+ instancia direto dessa classe [ex: new Personagem();], sendo possivel instacia apenas as
+ subclasse berseker,oraculo,cacador.
+ */
+public abstract class Personagem {
+    protected String nome;
+    protected byte pontosVida;
+    protected int ataque;
+    protected int defesa;
+    protected short nivel;
+    protected Inventario inventario;
+
+    public Personagem(String nome, byte pontosVida, int ataque, int defesa,short nivel){
+        this.nome = nome;
+        this.pontosVida = pontosVida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.nivel = nivel;
+        this.inventario = new Inventario(); // cada personagem vai ter o seu proprio inventario
+    }
 }
 
 
